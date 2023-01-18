d179e410e363ea29cfe416f2fb0e3af030128ceb
Brendon Mascarenhas <brendon_genssinger@hotmail.com>
2023-01-17 22:18:22 -0300

    Criação do Veiculo Teste

--- Estacionamento.Testes/VeiculoTeste.cs
+++ Estacionamento.Testes/VeiculoTeste.cs
@@ -4,33 +4,40 @@ using Xunit;
 
 namespace Estacionamento.Testes
 {
-    public class Tests
+    public class Tests 
     {
+        //Arrange
+        //Act
+        //Assert 
+
         [SetUp]
         public void Setup()
         {
         }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-
+      
         [Fact]
         public void TestaVeiculoAcelerar()
         {
+            //Arrange
             var veiculo = new Veiculo();
+
+            //Act
             veiculo.Acelerar(10);
 
+            //Assert
             Assert.Equals(100, veiculo.VelocidadeAtual);
         }
 
         [Fact]
         public void TestaVeiculoFreiar()
         {
+            //Arrange
             var veiculo = new Veiculo();
+
+            //Act
             veiculo.Frear(15);
+
+            //Assert
             Assert.Equals(-150, veiculo.VelocidadeAtual);
         }
     }
