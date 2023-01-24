98376d08f161ab2c496045638ed472c83f4cd552
Brendon Mascarenhas <brendon_genssinger@hotmail.com>
2023-01-24 15:32:32 -0300

    Ajustes da referencia do projeto.

---
+++ Estacionamento.Modelos/TipoVeiculo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estacionamento.Modelos
+{
+    public enum TipoVeiculo
+    {
+        Automovel = 0,
+        Motocicleta = 1
+    }
+}
